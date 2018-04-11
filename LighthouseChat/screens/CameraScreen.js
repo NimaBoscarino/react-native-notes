7ad03f890259c76394cd6f2fe2df802958726349
Nima Boscarino <nima.boscarino@gmail.com>
2018-04-10 20:57:11 -0700

    add camera screen stuff

--- LighthouseChat/screens/CameraScreen.js
+++ LighthouseChat/screens/CameraScreen.js
@@ -1,39 +1,95 @@
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
 
 export default class CameraScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasCameraPermission: null,
+      type: Camera.Constants.Type.back,
+    };
+
+    this.snap = this.snap.bind(this)
+  }
+
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
   };
 
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
 
+  async snap() {
+    console.log("hello world")
+
+    if (this.camera) {
+
+      let photo = await this.camera.takePictureAsync();
+
+      this.props.screenProps.updateData({
+        avatar: photo.uri
+      })
+
+      console.log(photo.uri)
+    }
+  };
+  
   render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.container}>
-          <Text>
-            Camera!
-          </Text>          
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  alignSelf: 'flex-start',
+                  alignItems: 'center',
+                }}
+                onPress={this.snap}
+                >
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Take Picture{' '}
+                </Text>
+              </TouchableOpacity>              
+            </View>
+          </Camera>
         </View>
-      </View>      
-    );
+      );
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'papayawhip',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-});
